f4e3187bd77e3ba7a0437a1252f851887f6ed5fe
Kylie Cuadra <45311838+kyliecuadra@users.noreply.github.com>
2022-02-23 14:51:49 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+	strings:["Computer Programmer", "Mobile Application Developer", "Java Programmer", "Python Programmer", "Web Developer", "Game Developer"],
+	typeSpeed: 80,
+	BackSpeed: 60,
+	loop:true
+})
