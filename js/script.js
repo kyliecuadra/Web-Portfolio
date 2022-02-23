ac6d405d11b0b7bec1b897a0d7506af937e43c3b
Kylie Cuadra <45311838+kyliecuadra@users.noreply.github.com>
2022-02-23 17:35:37 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing",{
-	strings:[" ", "Computer Programmer", "Mobile Application Developer", "Java Programmer", "Python Programmer", "Web Developer", "Game Developer"],
+	strings:["Computer Programmer", "Computer Programmer", "Mobile Application Developer", "Java Programmer", "Python Programmer", "Web Developer", "Game Developer"],
 	typeSpeed: 80,
 	BackSpeed: 60,
 	loop:true
